392f3d95b73083559e84f2f79461e4c79375f439
SeongBeom27 <lgh121546@gmail.com>
2020-02-25 04:08:58 -0800

    version1.1

--- ch1/hello/hello_client.c
+++ ch1/hello/hello_client.c
@@ -1,9 +1,13 @@
+/*
+	version 1.1
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+
 void error_handling(char *message);
 
 int main(int argc, char* argv[])
