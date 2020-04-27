53c0cc61cbbcba2a9f5ba1794e5d790938efa23c
SeongBeom27 <lgh121546@gmail.com>
2020-04-27 05:37:28 -0700

    ch10 finish

---
+++ ch10/echo_multiclnt.c
@@ -0,0 +1,86 @@
+/*
+	echo_multiclnt.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFSIZE 30
+
+void error_handling(char *message);
+
+int main(int argc, char* argv[])
+{
+    int sock;
+	pid_t pid;
+    char message[BUFSIZE];
+    int str_len, recv_len, recv_num;
+    struct sockaddr_in serv_addr;
+
+    if(argc != 3)
+    {
+        printf("Usage : %s <IP> <PORT>\n", argv[0]);
+        exit(1);
+    }
+    // make socket to access server
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+        error_handling("socket() error");
+
+
+    // input ip, port of server to struct object
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+    {
+        error_handling("connect() error");
+    }
+
+	pid = fork();
+	if(pid == 0)
+	{
+		while(1)
+		{
+			/* message input, transfer*/
+			fputs("Please input to transfer message (q to quit) : ", stdout);  // standard output
+			fgets(message, BUFSIZE, stdin);     // your input string put in message
+
+			if(!strcmp(message,"q\n"))
+			{
+				shutdown(sock, SHUT_WR);
+				close(sock);
+				exit(0);
+			}
+			write(sock, message, strlen(message));
+		}
+	}
+	else{
+		while(1)
+		{
+			int str_len = read(sock, message, BUFSIZE);
+			if(str_len == 0) exit(0);
+			message[str_len] = 0;
+			printf("Message from Server : %s\n", message);
+		}
+	}
+	close(sock);
+	return 0;
+}
+
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+       
+
